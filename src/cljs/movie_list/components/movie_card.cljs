f48cd7aa0d0e50215d961d6f25e194ec764ca854
Juho Leinonen <juholei@gmail.com>
2018-08-11 19:10:41 +0300

    Experimental blurred poster background on movie cards

--- src/cljs/movie_list/components/movie_card.cljs
+++ src/cljs/movie_list/components/movie_card.cljs
@@ -4,16 +4,20 @@
             [movie-list.events :as events]))
 
 (defn movie-card [movie-data]
-  [ui/card {:draggable     true
-            :on-drag-over  #(.preventDefault %)
-            :on-drag-start #(do (.setData (.-dataTransfer %) "text/plain" " ")
-                                (re-frame/dispatch [::events/set-dragged-item movie-data]))
-            :on-drag-end   #(re-frame/dispatch [::events/clear-dragged-item])
-            :on-drop       #(do (re-frame/dispatch [::events/set-order-list nil])
-                                (re-frame/dispatch [::events/drag-finished movie-data]))}
-   [ui/card-header {:title                  (:title movie-data)
-                    :subtitle               (:year movie-data)
-                    :avatar                 (:poster movie-data)
-                    :act-as-expander        true
-                    :show-expandable-button true}]
-   [ui/card-text {:expandable true} (:plot movie-data)]])
+  [:div.wrapper
+   [:div.blur {:style {:background-image (str "url(" (:poster movie-data) ")")}}]
+   [:div.content
+    [ui/card {:draggable     true
+              :on-drag-over  #(.preventDefault %)
+              :on-drag-start #(do (.setData (.-dataTransfer %) "text/plain" " ")
+                                  (re-frame/dispatch [::events/set-dragged-item movie-data]))
+              :on-drag-end   #(re-frame/dispatch [::events/clear-dragged-item])
+              :on-drop       #(do (re-frame/dispatch [::events/set-order-list nil])
+                                  (re-frame/dispatch [::events/drag-finished movie-data]))
+              :style {:background "transparent"}}
+     [ui/card-header {:title                  (:title movie-data)
+                      :subtitle               (:year movie-data)
+                      :avatar                 (:poster movie-data)
+                      :act-as-expander        true
+                      :show-expandable-button true}]
+     [ui/card-text {:expandable true} (:plot movie-data)]]]])
