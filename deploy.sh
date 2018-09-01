45721bcb88a31a7dff84d5f64237571778cc3220
Juho Leinonen <juholei@gmail.com>
2018-09-01 10:09:37 +0300

    Try installing aws-cli

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
+sed -i -e "s/(def omdb-api-key nil)/(def omdb-api-key \"$OMDBKEY\")/g" src/cljs/movie_list/config.cljs
 lein clean && lein cljsbuild once min && aws s3 sync --delete resources/public/ s3://$BUCKET/ --exclude 'js/compiled/out/*' --acl public-read
