3eba867c619a226256b0e6c00f76e77d11a8c25b
Juho Leinonen <juholei@gmail.com>
2018-12-19 19:12:41 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject movie-list "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"]
-                 [reagent "0.8.0"]
-                 [re-frame "0.10.5"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [reagent "0.8.1"]
+                 [re-frame "0.10.6"]
                  [cljs-react-material-ui "0.2.48"]
-                 [cljs-ajax "0.7.3"]
+                 [cljs-ajax "0.8.0"]
                  [day8.re-frame/http-fx "0.1.6"]
                  [camel-snake-kebab "0.4.0"]
                  [funcool/bide "1.6.0"]]
@@ -25,13 +25,13 @@
   :profiles
   {:dev
          {:dependencies [[binaryage/devtools "0.9.10"]
-                         [day8.re-frame/re-frame-10x "0.3.3"]
+                         [day8.re-frame/re-frame-10x "0.3.6"]
                          [day8.re-frame/tracing "0.5.1"]
                          [com.cemerick/piggieback "0.2.2"]
-                         [figwheel-sidecar "0.5.16"]]
+                         [figwheel-sidecar "0.5.17"]]
 
-          :plugins      [[lein-figwheel "0.5.16"]
-                         [lein-doo "0.1.10"]]
+          :plugins      [[lein-figwheel "0.5.17"]
+                         [lein-doo "0.1.11"]]
           :source-paths ["env/dev/clj"]}
    :prod {:dependencies [[day8.re-frame/tracing-stubs "0.5.1"]]}}
 
