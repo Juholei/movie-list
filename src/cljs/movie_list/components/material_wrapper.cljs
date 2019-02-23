35f5ab720b79a6dff7c86cda517b6f664df77728
Juho Leinonen <juholei@gmail.com>
2019-02-23 11:39:22 +0200

    Add Material-ui Reagent text-field fix from Reagent examples

--- src/cljs/movie_list/components/material_wrapper.cljs
+++ src/cljs/movie_list/components/material_wrapper.cljs
@@ -1,7 +1,49 @@
 (ns movie-list.components.material-wrapper
   (:require ["@material-ui/core" :as mui]
             ["@material-ui/core/styles" :refer [createMuiTheme withStyles]]
-            ["@material-ui/core/colors" :as mui-colors]))
+            ["@material-ui/core/colors" :as mui-colors]
+            [reagent.core :as r]
+            [goog.object :as gobj]
+            [reagent.impl.template :as rtpl]))
+
+;; TextField cursor fix:
+
+(def ^:private input-component
+  (r/reactify-component
+    (fn [props]
+      [:input (-> props
+                  (assoc :ref (:inputRef props))
+                  (dissoc :inputRef))])))
+
+(def ^:private textarea-component
+  (r/reactify-component
+    (fn [props]
+      [:textarea (-> props
+                     (assoc :ref (:inputRef props))
+                     (dissoc :inputRef))])))
+
+;; To fix cursor jumping when controlled input value is changed,
+;; use wrapper input element created by Reagent instead of
+;; letting Material-UI to create input element directly using React.
+;; Create-element + convert-props-value is the same as what adapt-react-class does.
+(defn text-field [props & children]
+  (let [props (-> props
+                  (assoc-in [:InputProps :inputComponent] (cond
+                                                            (and (:multiline props) (:rows props) (not (:maxRows props)))
+                                                            textarea-component
+
+                                                            ;; FIXME: Autosize multiline field is broken.
+                                                            (:multiline props)
+                                                            nil
+
+                                                            ;; Select doesn't require cursor fix so default can be used.
+                                                            (:select props)
+                                                            nil
+
+                                                            :else
+                                                            input-component))
+                  rtpl/convert-prop-value)]
+    (apply r/create-element mui/TextField props (map r/as-element children))))
 
 (defn dialog [opts content dialog-actions]
   [:> mui/Dialog opts
@@ -9,7 +51,7 @@
    [:> mui/DialogContent content]
    [:> mui/DialogActions dialog-actions]])
 ;
-(defn text-field [opts]
+#_(defn text-field [opts]
   [:> mui/TextField opts])
 ;
 (defn auto-complete [opts]
